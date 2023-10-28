58dc4db2dc343b11096c4246a3ae006e6da798a1
pRay01 <pray9563@gmail.com>
2023-10-28 12:23:46 +0530

    google analytics test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
-import Provider from "../common/Provider"
+import Provider from "../common/Provider";
 import Footer from "@/components/Footer";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,9 +24,20 @@ export default function RootLayout({
     <html lang="en">
       <body className={"3xl:px-[20%] 2xl:px-0"}>
         <Provider>
-        <Header />
-        {children}
-        <Footer/>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-08497J1RLM"
+          ></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {` window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-08497J1RLM');`}
+          </Script>
+          <Header />
+          {children}
+          <Footer />
         </Provider>
       </body>
     </html>
