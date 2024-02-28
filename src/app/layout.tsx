1baecd61ba9274b419944344e23fb019fdbc949d
zero <pray9563@gmail.com>
2024-02-28 13:56:07 +0530

    added chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,23 @@ export default function RootLayout({
       <body className={"3xl:px-[20%] 2xl:px-0"}>
         <Provider>
           <Script
+          id="tawk_api"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/65dedfbb9131ed19d972b0d1/1hnn9jtq5';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          ></Script>
+          <Script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3654781869371720"
             crossOrigin="anonymous"
